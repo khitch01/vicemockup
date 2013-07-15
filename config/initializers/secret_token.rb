7d1c24ad03e8964f0faa21b1fcd2053a22b4617d
Katherine Hitchner <k.hitchner09@gmail.com>
2013-07-15 11:32:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vicemockup::Application.config.secret_token = 'df957d2f505ec90a3fa6e67d277659c546c39d5a4184b2f3586d73baf76ff1751ae0ec8cca8f0536b1cf7b391e48bc88a7e9eda29c9462cd119641c0e3459591'
